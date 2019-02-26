c2659f7cc1dd9706d6b20451199d0883c695c45e
WinnieWei0 <1344228500@qq.com>
2019-02-26 17:53:28 +0800

    添加入数据库完成

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-let request = require('request')
-// let mysql=require('mysql')
-let jsdom = require('jsdom')
-let {
-  JSDOM
-} = jsdom
+const request = require('request')
+const mysql = require('mysql')
+const jsdom = require('jsdom')
+const {JSDOM} = jsdom
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'node_reptile'
+})
 
 request.get('https://www.csdn.net/', (err, res) => {
   if (err) {
     throw err
   }
-  let {
-    window
-  } = new JSDOM(res.body)
-  let $ = require('jquery')(window)
-    let items=[]
+  const {window} = new JSDOM(res.body)
+  const $ = require('jquery')(window)
   $(function () {
+  const {window} = new JSDOM(res.body)
+  const $ = require('jquery')(window)
+  let item = 0
     $('.feedlist_mod>li').each(function (i) {
       let title = $(this).find('.title>h2>a').text().trim()
       let title_href = $(this).find('.title>h2>a').attr('href')
@@ -28,20 +32,30 @@ request.get('https://www.csdn.net/', (err, res) => {
       let tag_time = $(this).find('.list_userbar>.time').text().trim()
       let read_num = $(this).find('.list_userbar>.interactive>.read_num>a>.num').text().trim()
       let common_num = $(this).find('.list_userbar>.interactive>.common_num>a>.num').text().trim()
-      items.push({
-        title
+      item++
+      db.query(`select * from home_feed where title='${title}'`, (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        if (data.length < 1) {
+          db.query(`insert into home_feed (title,title_href,summary,user_img_href,user_img,name_href,name,tag_href,tag,tag_time,read_num,common_num) value('${title}','${title_href}','${summary}','${user_img_href}','${user_img}','${name_href}','${name}','${tag_href}','${tag}','${tag_time}','${read_num}','${common_num}')`, (err, data) => {
+            if (err) {
+              console.log(err)
+            }
+          })
+        }
       })
     })
-    $('.company_list>li').each(function (i) {
-      let img_href = $(this).find('.img_box>a').attr('href')
-      let img_src = $(this).find('.img_box>a>img').attr('src')
-      let content_href = $(this).find('.content>h3>a').attr('href')
-      let content_txt = $(this).find('.content>h3>a').text().trim()
-      let content_desc = $(this).find('.content>h3>p').text().trim()
-      // items.push({
-      //   title
-      // })
-    })
-    console.log(items)
+    // $('.company_list>li').each(function (i) {
+    //   let img_href = $(this).find('.img_box>a').attr('href')
+    //   let img_src = $(this).find('.img_box>a>img').attr('src')
+    //   let content_href = $(this).find('.content>h3>a').attr('href')
+    //   let content_txt = $(this).find('.content>h3>a').text().trim()
+    //   let content_desc = $(this).find('.content>h3>p').text().trim()
+    //   // items.push({
+    //   //   title
+    //   // })
+    // })
+    console.log('item:', item)
   })
 })
